72a8207ce8d55acee8ee9a720e3d2593c1b08b5e
tprax <tprax01@gmail.com>
2018-09-17 16:16:34 -0600

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,16 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Menu from './components/Menu';
 import NoMatch from './components/NoMatch'
+import NavBar from './components/NavBar'
 
 const App = () => (
   <Fragment>
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/menu" component={Menu}/>
-      <Route component={NoMatch}/>
-    </Switch>
+    <NavBar/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/menu" component={Menu}/>
+        <Route component={NoMatch}/>
+      </Switch>
   </Fragment>
 )
 
